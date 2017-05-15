d556f94cab41bb4c731fece84a93ef55f0581181
huchim <huchim@live.com.mx>
2017-05-15 17:02:53 -0500

    Initial commit
    :D

---
+++ src/HtmlGenerator.cs
@@ -0,0 +1,145 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="HtmlGenerator.cs" company="Carlos Huchim Ahumada">
+// Este código se libera bajo los términos de licencia especificados.
+// </copyright>
+// -----------------------------------------------------------------------
+namespace Jaguar.Reporting.Generators
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using Jaguar.Reporting.Common;
+    using Mustache;
+
+
+    /// <summary>
+    /// Genera la información en formato CSV.
+    /// </summary>
+    public class HtmlGenerator : IGeneratorEngine
+    {
+        private ReportHandler report;
+        private Dictionary<string, object> variables;
+
+        /// <inheritdoc/>
+        public string FileExtension => ".html";
+
+        /// <inheritdoc/>
+        public Guid Id => new Guid("eeb51011-ee09-425d-9473-3ee4192b9e2e");
+
+        /// <inheritdoc/>
+        public bool IsEmbed => true;
+
+        /// <inheritdoc/>
+        public string MimeType => "text/html";
+
+        /// <inheritdoc/>
+        public string Name => "Página HTML";
+
+        private string TemplateFile
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(this.report.Options["html.template"] as string))
+                {
+                    throw new ArgumentNullException(nameof(this.TemplateFile));
+                }
+
+                return Path.Combine(this.report.WorkDirectory, this.report.Options["html.template"].ToString());
+            }
+        }
+
+        /// <inheritdoc/>
+        public byte[] GetAllBytes(ReportHandler report, List<DataTable> data, Dictionary<string, object> variables)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc/>
+        public string GetString(ReportHandler report, List<DataTable> data, Dictionary<string, object> variables)
+        {
+            // Asignar variables de la clase.
+            this.report = report;
+            this.variables = variables;
+
+            // Cargar el archivo en memoria.
+            var htmlTemplate = this.LoadTemplateFile();
+
+            // Reemplazar variables.
+            htmlTemplate = this.ReplaceDataVariables(htmlTemplate, variables);
+
+            // Crear el compilador y agregar una etiqueta personalizada.
+            var compiler = new HtmlFormatCompiler();
+            compiler.RegisterTag(new IfEqualTagDefinition(), true);
+
+            // Compilar la plantilla.
+            var generator = compiler.Compile(htmlTemplate);
+
+            // Unificar los resultados.
+            var mergedData = this.MergeData(data);
+
+            // Generar los resultados.
+            string result = generator.Render(mergedData);
+
+
+            return result;
+        }
+
+        /// <summary>
+        /// Carga el texto de la plantilla.
+        /// </summary>
+        /// <returns></returns>
+        private string LoadTemplateFile()
+        {
+            var templateFile = Path.Combine(this.report.WorkDirectory, this.TemplateFile);
+            return File.ReadAllText(templateFile);
+        }
+
+        private Dictionary<string, List<Dictionary<string, object>>> MergeData(List<DataTable> data)
+        {
+            var c = new Dictionary<string, List<Dictionary<string, object>>>();
+
+            foreach (var table in data)
+            {
+                // Agregar la llave para poder usarla.
+                c.Add(table.TableName, new List<Dictionary<string, object>>());
+
+                // Hacer referencia al nodo.
+                var m = c[table.TableName];
+
+                foreach (var dataRow in table.Rows)
+                {
+                    var row = new Dictionary<string, object>();
+
+                    foreach (var column in dataRow.Columns)
+                    {
+                        if (column.Value is string)
+                        {
+                            column.Value = column.Value.ToString();
+                        }
+
+                        if (column.Value is DBNull)
+                        {
+                            column.Value = null;
+                        }
+
+                        row.Add(column.Name, column.Value);
+                    }
+
+                    m.Add(row);
+                }
+            }
+
+            return c;
+        }
+
+        private string ReplaceDataVariables(string input, Dictionary<string, object> variables)
+        {
+            foreach (var variable in variables)
+            {
+                input = input.Replace($"%{variable.Key}%", variable.Value.ToString());
+            }
+
+            return input;
+        }
+    }
+}
---
+++ src/IfEqualTagDefinition.cs
@@ -0,0 +1,77 @@
+﻿namespace Jaguar.Reporting.Generators
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using Mustache;
+
+    internal sealed class IfEqualTagDefinition : ContentTagDefinition
+    {
+        private const string conditionParameter = "condition";
+        private const string comparerParameter = "comparer";
+
+        public IfEqualTagDefinition() : base("ifeq")
+        {
+        }
+
+        /// <summary>
+        /// Gets the tags that come into scope within the context of the current tag.
+        /// </summary>
+        /// <returns>The child tag definitions.</returns>
+        protected override IEnumerable<string> GetChildTags()
+        {
+            return new string[] { "elif", "else" };
+        }
+
+        /// <summary>
+        /// Gets the parameters that can be passed to the tag.
+        /// </summary>
+        /// <returns>The parameters.</returns>
+        protected override IEnumerable<TagParameter> GetParameters()
+        {
+            return new[]
+            {
+                new TagParameter(conditionParameter) { IsRequired = true },
+                new TagParameter(comparerParameter) { IsRequired = true },
+            };
+        }
+
+        /// <summary>
+        /// Gets the parameters that are used to create a new child context.
+        /// </summary>
+        /// <returns>The parameters that are used to create a new child context.</returns>
+        public override IEnumerable<TagParameter> GetChildContextParameters()
+        {
+            return new TagParameter[0];
+        }
+
+        /// <summary>
+        /// Gets whether the given tag's generator should be used for a secondary (or substitute) text block.
+        /// </summary>
+        /// <param name="definition">The tag to inspect.</param>
+        /// <returns>True if the tag's generator should be used as a secondary generator.</returns>
+        public override bool ShouldCreateSecondaryGroup(TagDefinition definition)
+        {
+            return new string[] { "elif", "else" }.Contains(definition.Name);
+        }
+
+        /// <summary>
+        /// Gets whether the primary generator group should be used to render the tag.
+        /// </summary>
+        /// <param name="arguments">The arguments passed to the tag.</param>
+        /// <returns>
+        /// True if the primary generator group should be used to render the tag;
+        /// otherwise, false to use the secondary group.
+        /// </returns>
+        public override bool ShouldGeneratePrimaryGroup(Dictionary<string, object> arguments)
+        {
+            object condition = arguments[conditionParameter];
+            object comparer = arguments[comparerParameter];
+            return isConditionSatisfied(condition, comparer);
+        }
+
+        private bool isConditionSatisfied(object condition, object comparer)
+        {
+            return condition.Equals(comparer);
+        }
+    }
+}
